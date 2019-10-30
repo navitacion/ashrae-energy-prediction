36f3ff17e663cd2c088ec9a3f1c62f445ef3d0be
MasatakaImamura <masataka.i.navi@gmail.com>
2019-10-30 20:22:46 +0900

    run.pyはpickleモードだけにする

--- Run/Run.py
+++ Run/Run.py
@@ -17,41 +17,16 @@ Sampling_rate = None
 SEED = 42
 chunk_size = 50000
 today = datetime.datetime.now().strftime('%Y%m%d')
-use_pickle = True
 use_col_nums = 40
 set_cols = pd.read_csv('../Importance/importance_20191029.csv')['feature'][:use_col_nums].tolist()
 train_set_cols = set_cols + ['meter_reading']
 
 # Prep Train Data  #####################################################################
 
-# Full Scratch Preprocessing
-if not use_pickle:
-    print('Data Loading...')
-    train = pd.read_csv("../input/train.csv")
-    df_weather_train = pd.read_csv("../input/weather_train.csv")
-    df_building = pd.read_csv("../input/building_metadata.csv")
-
-    # Sampling
-    if Sampling_rate is not None:
-        train = train.sample(frac=Sampling_rate, random_state=SEED)
-
-    # Prepare Train Data
-    Dataset = PreprocessingDataset()
-    Dataset.prep(train, df_weather_train, df_building, mode='train')
-    # Save Preprocessed Train Data
-    with open('../input/prep_train.pkl', 'wb') as f:
-        pickle.dump(Dataset, f, protocol=4)
-    # Memory Clear
-    del train, df_weather_train, df_building
-    gc.collect()
-
 # Load Pkl File  #####################################################################
-if use_pickle:
-    print('Load Pkl File...')
-    with open('../input/prep_train.pkl', 'rb') as f:
-        Dataset = pickle.load(f)
-
-print('Data Already...')
+print('Create Model...')
+with open('../input/prep_train.pkl', 'rb') as f:
+    Dataset = pickle.load(f)
 
 # Model Create  #####################################################################
 model = Trainer()
@@ -61,27 +36,11 @@ with open(f'../Model/lgb_models_{today}.pkl', 'wb') as f:
     pickle.dump(model.models, f)
 
 # Prediction  #####################################################################
-print('Prediction')
-_start = time.time()
-if not use_pickle:
-    df_weather_test = pd.read_csv("../input/weather_test.csv")
-    df_building = pd.read_csv("../input/building_metadata.csv")
-
-    # Prepare Test Data
-    test = pd.read_csv("../input/test.csv")
-    Dataset.prep(test, df_weather_test, df_building, mode='test')
-    # Save Preprocessed Train Data
-    with open('../input/prep_test.pkl', 'wb') as f:
-        pickle.dump(Dataset, f, protocol=4)
-    # Memory Clear
-    del test, df_weather_test, df_building
-    gc.collect()
 
 # Load Pkl File  #####################################################################
-if use_pickle:
-    print('Load Pkl File...')
-    with open('../input/prep_test.pkl', 'rb') as f:
-        Dataset = pickle.load(f)
+print('Prediction...')
+with open('../input/prep_test.pkl', 'rb') as f:
+    Dataset = pickle.load(f)
 
 # Pred  #####################################################################
 pred_all = model.predict(Dataset.df[set_cols], step_size=chunk_size)
